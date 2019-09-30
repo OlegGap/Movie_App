35c1296305057426f03527c00b1ac8be56c29243
oleg <hnapovskiy20@gmail.com>
2019-09-30 17:56:55 +0300

    Updates

---
+++ precache-manifest.482375b97ba13aec29136c608665ad95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9462be0b16e98805341000a83fbb3aa3",
+    "url": "/Movie_App/static/media/my.9462be0b.jpg"
+  },
+  {
+    "revision": "875e4174d8c3af7acc20bb2bda5ab0bf",
+    "url": "/Movie_App/static/media/logo.875e4174.svg"
+  },
+  {
+    "revision": "a46080ad9491146ffa92",
+    "url": "/Movie_App/static/js/runtime~main.b6476f2f.js"
+  },
+  {
+    "revision": "1979cb4f63ae90cf180f",
+    "url": "/Movie_App/static/js/main.485f7e04.chunk.js"
+  },
+  {
+    "revision": "769842f758fe98630f9a",
+    "url": "/Movie_App/static/js/2.42b44dd6.chunk.js"
+  },
+  {
+    "revision": "1979cb4f63ae90cf180f",
+    "url": "/Movie_App/static/css/main.6cf55b0c.chunk.css"
+  },
+  {
+    "revision": "769842f758fe98630f9a",
+    "url": "/Movie_App/static/css/2.f2579b62.chunk.css"
+  },
+  {
+    "revision": "c768a7f2d761ce6effb7c4482ca4e2f1",
+    "url": "/Movie_App/index.html"
+  }
+];
